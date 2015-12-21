1143b2ef7e982bc3031945ee62772a8a8228ccdf
backfrip <backfrip@users.noreply.github.com>
2015-12-21 17:58:28 +0000

    Basic configuration... dude.

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+import os.path
+
+app = Flask(__name__)
+
+def index():
+    return '<h1>Index</h1>'
+
+if __name__ == '__main__':
+    app.debug = True
+    app.secret_key = '895uvq_09ta834_xna_2847vt3v9o3u8tw948t5e'
+    app.run('0.0.0.0',
+        8080 if os.path.isfile('./cloudy') else 8000)
