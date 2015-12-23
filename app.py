5946f27176699a1e27d2f94b09547affece9fee8
TESLA <57leonardo@gmail.com>
2015-12-23 13:24:06 -0500

    starting some assisted-search code

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
-from flask import Flask, render_template
-import os.path
+from flask import Flask, render_template, request, redirect, session, url_for
+import os.path, google, urllib2, bs4, re
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route("/search")
+def search():
+    q = request.args.get('search')
+    text = None
+    message = None
+    if q:
+        q = q.strip()
+        text = ''
+        for n in range(5):
+            url = google.search(q, num = 5, start = 0, stop = 5, pause=2.0).next()
+            page = google.get_page(url)
+            print(url);
+            soup = bs4.BeautifulSoup(page)
+    return render_template("search.html")
+
 
 if __name__ == '__main__':
     app.debug = True
