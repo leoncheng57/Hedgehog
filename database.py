482de586ab256cd1a467013c9b47d1c6a7767751
backfrip <backfrip@users.noreply.github.com>
2016-01-09 00:18:46 +0000

    Having to find everything seems a bit silly. 'Yo Mongo! Where my data be at?' 'Oh, uh... lemme just find it...' 'Bruh pls.'

--- database.py
+++ database.py
@@ -25,7 +25,22 @@ def create_tag(name, info_list=[], parent_tags=[], child_tags=[]):
     return True
 
 def get_all_tags():
-    return tags.find_many()
+    return list(tags.find(projection=['name']))
+
+def get_top_level_tags():
+    return list(tags.find({'parents': []}, ['name']))
+
+def get_parent_tags(child_id):
+    parents = []
+    for parent_id in tags.find_one(child_id)['parents']:
+        parents.append(tags.find_one(parent_id, ['name']))
+    return parents
+
+def get_child_tags(parent_id):
+    children = []
+    for child_id in tags.find_one(parent_id)['children']:
+        children.append(tags.find_one(child_id, ['name']))
+    return children
 
 # Info related functions
 def create_info(title, body, author_id, tag_list):
@@ -64,17 +79,17 @@ def verify_user(username, password):
 # Linkage functions
 def link_tag_to_info(tag_id, info_list):
     for info_id in info_list:
-        info.find_one_and_update({'_id': info_id}, {'$addToSet': {'tags': tag_id}})
+        info.find_one_and_update(info_id, {'$addToSet': {'tags': tag_id}})
 
 def link_tag_to_tags(tag_id, parent_tags=[], child_tags=[]):
     for ptag_id in parent_tags:
-        tags.find_one_and_update({'_id': ptag_id}, {'$addToSet': {'children': tag_id}})
+        tags.find_one_and_update(ptag_id, {'$addToSet': {'children': tag_id}})
     for ctag_id in child_tags:
-        tags.find_one_and_update({'_id': ctag_id}, {'$addToSet': {'parents': tag_id}})
+        tags.find_one_and_update(ctag_id, {'$addToSet': {'parents': tag_id}})
 
 def link_info_to_tags(info_id, tag_list):
     for tag_id in tag_list:
-        tags.find_one_and_update({'_id': tag_id}, {'$addToSet': {'info': info_id}})
+        tags.find_one_and_update(tag_id, {'$addToSet': {'info': info_id}})
 
 def link_info_to_user(info_id, user_id):
-    users.find_one_and_update({'_id': user_id}, {'$addToSet': {'info': info_id}})
+    users.find_one_and_update(user_id, {'$addToSet': {'info': info_id}})
