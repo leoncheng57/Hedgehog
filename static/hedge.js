a184a45022764e733cf720063587f29b37a1f61f
angelachan64 <angelachan64@gmail.com>
2016-01-11 18:22:09 +0000

    making a login box

--- static/hedge.js
+++ static/hedge.js
@@ -1,17 +1,22 @@
 $(document).ready(function() {
     console.log("loaded");
     $(".intro").hide().fadeIn(1000);
+    $(".login").hide();
 });
 
 function homepage(){
+    $(".login").fadeOut(1000);
     $(".intro").fadeIn(1000);
 }
 function signin(){
     $(".intro").fadeOut(1000);
+    $(".login").fadeIn(1000);
 }
 function signup(){
+    $(".login").fadeOut(1000);
     $(".intro").fadeOut(1000);
 }
 function about(){
+    $(".login").fadeOut(1000);
     $(".intro").fadeOut(1000);
 }
