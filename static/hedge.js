fd0e661e275e4e073936dc25c45a8a83a8c46841
angelachan64 <angelachan64@gmail.com>
2016-01-12 23:41:41 +0000

    forgot to push signup box

--- static/hedge.js
+++ static/hedge.js
@@ -2,21 +2,26 @@ $(document).ready(function() {
     console.log("loaded");
     $(".intro").hide().fadeIn(1000);
     $(".login").hide();
+    $(".signup").hide();
 });
 
 function homepage(){
     $(".login").fadeOut(1000);
+    $(".signup").fadeOut(1000);
     $(".intro").fadeIn(1000);
 }
 function signin(){
     $(".intro").fadeOut(1000);
+    $(".signup").fadeOut(1000);
     $(".login").fadeIn(1000);
 }
 function signup(){
     $(".login").fadeOut(1000);
     $(".intro").fadeOut(1000);
+    $(".signup").fadeIn(1000);
 }
 function about(){
     $(".login").fadeOut(1000);
     $(".intro").fadeOut(1000);
+    $(".signup").fadeOut(1000);
 }
