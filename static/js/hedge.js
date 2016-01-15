2f20a59eeabd004b0d996f17035f29dfeea41512
angelachan64 <angelachan64@gmail.com>
2016-01-15 17:52:33 +0000

    testing for Amanda set up

--- static/js/hedge.js
+++ static/js/hedge.js
@@ -3,18 +3,24 @@ $(".intro").fadeIn(1000);
 $('#logo').click( function homepage() {
     $(".login").fadeOut(1000);
     $(".signup").fadeOut(1000);
+    $(".about").fadeOut(1000);
+    $(".team").fadeOut(1000);
     $(".intro").fadeIn(1000);
 });
 
 $('#signin').click( function signin() {
     $(".intro").fadeOut(1000);
     $(".signup").fadeOut(1000);
+    $(".about").fadeOut(1000);
+    $(".team").fadeOut(1000);
     $(".login").fadeIn(1000);
 });
 
 $('#signup').click( function signup() {
     $(".login").fadeOut(1000);
     $(".intro").fadeOut(1000);
+    $(".about").fadeOut(1000);
+    $(".team").fadeOut(1000);
     $(".signup").fadeIn(1000);
 });
 
@@ -22,4 +28,14 @@ $('#about').click( function about() {
     $(".login").fadeOut(1000);
     $(".intro").fadeOut(1000);
     $(".signup").fadeOut(1000);
+    $(".team").fadeOut(1000);
+    $(".about").fadeIn(1000);
+});
+
+$('#team').click( function team() {
+    $(".login").fadeOut(1000);
+    $(".intro").fadeOut(1000);
+    $(".signup").fadeOut(1000);
+    $(".about").fadeOut(1000);
+    $(".team").fadeIn(1000);
 });
