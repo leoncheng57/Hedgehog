9aef7373a6ab0add5ce39c6c65419b3725938d11
backfrip <backfrip@users.noreply.github.com>
2016-01-26 19:29:34 +0000

    Cleanup 2.

--- static/js/in.js
+++ static/js/in.js
@@ -1,30 +1,27 @@
-$(document).ready(function () {
-  var trigger = $('.hamburger'),
-      overlay = $('.overlay'),
-     isClosed = false;
+var trigger = $('.hamburger'),
+  overlay = $('.overlay'),
+  isClosed = false;
 
-    trigger.click(function () {
-      hamburger_cross();      
-    });
+trigger.click( function() {
+  hamburger_cross();
+});
 
-    function hamburger_cross() {
-
-      if (isClosed == true) {          
-        overlay.hide();
-        trigger.removeClass('is-open');
-        trigger.addClass('is-closed');
-        isClosed = false;
-        $(".footer").fadeOut();
-      } else {   
-        overlay.show();
-        trigger.removeClass('is-closed');
-        trigger.addClass('is-open');
-        isClosed = true;
-        $(".footer").fadeIn();
-      }
+function hamburger_cross() {
+  if (isClosed == true) {
+    overlay.hide();
+    trigger.removeClass('is-open');
+    trigger.addClass('is-closed');
+    isClosed = false;
+    $(".footer").fadeOut();
+  } else {
+    overlay.show();
+    trigger.removeClass('is-closed');
+    trigger.addClass('is-open');
+    isClosed = true;
+    $(".footer").fadeIn();
   }
+}
   
-  $('[data-toggle="offcanvas"]').click(function () {
-        $('#wrapper').toggleClass('toggled');
-  });  
+$('[data-toggle="offcanvas"]').click( function() {
+  $('#wrapper').toggleClass('toggled');
 });
--- static/js/info.js
+++ static/js/info.js
@@ -1,53 +1,42 @@
 var createInfo = $('#create-info');
 var showButton = $('#showButton');
-
-if (createInfo) createInfo.submit( function(e) {
-    e.preventDefault();
-    var form = {};
-    $.each( $(this).serializeArray(), function(i, e) {
-	form[e.name] = e.value;
-    });
-    $.ajax({
-	type: 'POST',
-	url: '/api/info/create/',
-	data: JSON.stringify(form),
-	contentType: 'application/json',
-	accepts: 'application/json',
-	success: function(data) {
-	    console.log(data);
-	    
-	    // $('#' + type + '-search-results').empty();				       
-	    // $.each(data, function(i, item) {					       
-	    //     $('#' + type + '-search-results').append(			       
-	    // 	  $('<li>').append(item.name + ' (' + item._id.$oid + ')'));	       
-	    // });								       
-	    
-	}
-    });
-});
-
-if (showButton) showButton.click( function(e) {
-    console.log("displaying the infos");
-    $('<form action="/api/info/display_infos/" method="POST">').submit();
-});
-
-function myFunction() {
-    document.getElementById("myP").contentEditable = true;
-    //document.getElementById("demo").innerHTML = "STUDY GUIDE IN PROGRESS...";
-}
-function stop(){
-	document.getElementById("myP").contentEditable = false;
-}
-
-
-
-
-
 var tagsTable = $('#tags');
 var infoTable = $('#info');
 
-getTags();
-getInfo();
+if (tagsTable) getTags();
+if (infoTable) getInfo();
+
+if (createInfo) createInfo.submit( function(e) {
+  e.preventDefault();
+  var form = {};
+  $.each( $(this).serializeArray(), function(i, e) {
+	  form[e.name] = e.value;
+  });
+  $.ajax({
+    type: 'POST',
+    url: '/api/info/create/',
+    data: JSON.stringify(form),
+    contentType: 'application/json',
+    accepts: 'application/json',
+    success: function(data) {
+      console.log(data);
+    }
+  });
+});
+
+if (showButton) showButton.click( function(e) {
+  console.log("displaying the infos");
+  $('<form action="/api/info/display_infos/" method="POST">').submit();
+});
+
+function myFunction() {
+    $('#myP').contentEditable = true;
+    //document.getElementById("demo").innerHTML = "STUDY GUIDE IN PROGRESS...";
+}
+
+function stop(){
+	$('#myP').contentEditable = false;
+}
 
 function getTags() {
   $.ajax({
