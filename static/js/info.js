2c83251c618b97d291119c7080b44c5bdd696955
TESLA <57leonardo@gmail.com>
2016-01-25 22:29:47 -0500

    moved script container to info.js

--- static/js/info.js
+++ static/js/info.js
@@ -1,33 +1,42 @@
 var createInfo = $('#create-info');
-
-if (createInfo) createInfo.submit( function(e) {
-	e.preventDefault();
-	var form = {};
-	$.each( $(this).serializeArray(), function(i, e) {
-		form[e.name] = e.value;
-	});
-	$.ajax({
-		type: 'POST',
-		url: '/api/info/create/',
-		data: JSON.stringify(form),
-		contentType: 'application/json',
-		accepts: 'application/json',
-		success: function(data) {
-			console.log(data);
-			///////////////////////////////////////////////////////////////////////////////
-			// $('#' + type + '-search-results').empty();				       //
-			// $.each(data, function(i, item) {					       //
-			//     $('#' + type + '-search-results').append(			       //
-			// 	  $('<li>').append(item.name + ' (' + item._id.$oid + ')'));	       //
-			// });								       //
-			///////////////////////////////////////////////////////////////////////////////
-		}
-	});
-});
-
 var showButton = $('#showButton');
 
-if (showButton) showButton.click( function(e) {
-  console.log("displaying the infos");
-  $('<form action="/api/info/display_infos/" method="POST">').submit();
+if (createInfo) createInfo.submit( function(e) {
+    e.preventDefault();
+    var form = {};
+    $.each( $(this).serializeArray(), function(i, e) {
+	form[e.name] = e.value;
+    });
+    $.ajax({
+	type: 'POST',
+	url: '/api/info/create/',
+	data: JSON.stringify(form),
+	contentType: 'application/json',
+	accepts: 'application/json',
+	success: function(data) {
+	    console.log(data);
+	    
+	    // $('#' + type + '-search-results').empty();				       
+	    // $.each(data, function(i, item) {					       
+	    //     $('#' + type + '-search-results').append(			       
+	    // 	  $('<li>').append(item.name + ' (' + item._id.$oid + ')'));	       
+	    // });								       
+	    
+	}
+    });
 });
+
+if (showButton) showButton.click( function(e) {
+    console.log("displaying the infos");
+    $('<form action="/api/info/display_infos/" method="POST">').submit();
+});
+
+function myFunction() {
+    document.getElementById("myP").contentEditable = true;
+    //document.getElementById("demo").innerHTML = "STUDY GUIDE IN PROGRESS...";
+}
+function stop(){
+	document.getElementById("myP").contentEditable = false;
+}
+
+
